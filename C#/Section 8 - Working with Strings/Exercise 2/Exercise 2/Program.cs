21d6623345e9ba2548e0c3cc574cf5713f30012d
bionicbread <christopheryu87@gmail.com>
2022-02-03 22:39:21 +0000

    Exercise 2 complete

---
+++ C#/Section 8 - Working with Strings/Exercise 2/Exercise 2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+//using System.Linq;
+using System.Collections.Generic;
+
+namespace Exercise_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //2- Write a program and ask the user to enter a few numbers separated by a hyphen.
+            //If the user simply presses Enter, without supplying an input, exit immediately; otherwise, check to see if there are duplicates.
+            //If so, display "Duplicate" on the console.
+
+            List<int> listFormat = new List<int>();
+            //bool isDuplicate;
+            
+            int ender = 0;
+            do
+            {
+                Console.WriteLine("Input some numbers that is hypen separated - Duplicates will be detected");
+                Console.WriteLine("To quit enter no values");
+                string input = Console.ReadLine();
+
+                if (input == "")
+                {
+                    break;
+                }
+
+                string[] numbers = input.Split('-');
+                //int[] intArray = new int[numbers.Length];
+                int[] intArray = Array.ConvertAll(numbers, int.Parse);
+
+                foreach (int n in intArray)
+                    listFormat.Add(n);
+
+                foreach (int k in listFormat)
+                   
+                    if (listFormat.IndexOf(k) == listFormat.LastIndexOf(k))
+                    {
+                        continue;
+                    }
+                    else if (listFormat.IndexOf(k) != listFormat.LastIndexOf(k))
+                    {
+                        Console.WriteLine("DUPLICATE");
+                        ender++;
+                        break;
+                    }
+      
+                 
+            }
+            while (ender == 0);
+
+        }
+    }
+}
+
+
+
--- C#/Section 8 - Working with Strings/Exercise1/Exercise1/SequenceLogic.cs
+++ C#/Section 8 - Working with Strings/Exercise1/Exercise1/SequenceLogic.cs
@@ -26,20 +26,23 @@ namespace Exercise1
             {
                 conCheck += k; // sums up the numbers in the array
             }
-            double sumOfArray = (numberArray.Length / 2f * ((2 * numberArray[0]) + ((numberArray.Length) - 1) * 1)); // arithmetric series in the postive
-            double sumOfNegArray = numberArray.Length / 2f * (numberArray[0] + numberArray[numberArray.Length - 1]); //arithmetric series in the negative
-            Console.WriteLine(conCheck);
-            Console.WriteLine("Pos series: " + sumOfArray);
-            Console.WriteLine("Neg series: " + sumOfNegArray);
-
+            double sumOfArray = numberArray.Length / 2f * (numberArray[0] + numberArray[numberArray.Length - 1]); // arithmetric series in the postive
+            //////////////////////DONT NEED THIS!!///////////////////////////
+            //double sumOfNegArray = numberArray.Length / 2f * (numberArray[0] + numberArray[numberArray.Length - 1]); //arithmetric series in the negative 
+            //Console.WriteLine(conCheck);
+            //Console.WriteLine("Pos series: " + sumOfArray);
+            //Console.WriteLine("Neg series: " + sumOfNegArray);
+            ////////////////////DONT NEED THIS!!////////////////////////////
             if (conCheck == sumOfArray) // if consecutive in the positive
             {
                 Console.WriteLine("Consecutive");
             }
-            else if (conCheck == (sumOfNegArray)) // if consecutive in the negative
-            {
-                Console.WriteLine("Consecutive");
-            }
+            ////////////////////DONT NEED THIS!!////////////////////////////
+            //else if (conCheck == (sumOfNegArray)) // if consecutive in the negative
+            //{
+            //    Console.WriteLine("Consecutive");
+            //}
+            ////////////////////DONT NEED THIS!!////////////////////////////
             else if (conCheck != sumOfArray) // not consecutive 
             {
                 Console.WriteLine("Not Consecutive");
