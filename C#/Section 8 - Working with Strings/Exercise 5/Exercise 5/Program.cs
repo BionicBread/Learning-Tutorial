047a7b3ffb43ec3d96815af27d24c65ebf50e405
bionicbread <christopheryu87@gmail.com>
2022-02-06 15:06:15 +0000

    Exercise 5 completed - gg EZ Exercise 3 was completed in last commit

---
+++ C#/Section 8 - Working with Strings/Exercise 5/Exercise 5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+
+namespace Exercise_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //5- Write a program and ask the user to enter an English word.
+            //Count the number of vowels (a, e, o, u, i) in the word.
+            //So, if the user enters "inadequate", the program should display 6 on the console.
+
+            Console.WriteLine("Enter an English word, the number of vowels will be counted");
+            string input = Console.ReadLine();
+          
+            Console.WriteLine("Number of vowels: " + VowelCounter.CounterOfVowels(input));
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ C#/Section 8 - Working with Strings/Exercise 5/Exercise 5/VowelCounter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_5
+{
+    public static class VowelCounter
+    {
+        public static int CounterOfVowels(string input)
+        {
+            int numberOfVowels = 0;
+            List<char> charMod = new List<char>();
+            foreach (char c in input)
+            {
+
+                charMod.Add(c);
+            }
+
+            foreach (char s in charMod)
+            {
+                if (s == ('a') || s == ('e') || s == ('o') || s == ('u') || s == ('i'))
+                {
+                    numberOfVowels++;
+                }
+
+            }
+            return numberOfVowels;
+        }
+    }
+}
