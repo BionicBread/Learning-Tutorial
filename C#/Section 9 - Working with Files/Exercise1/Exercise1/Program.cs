1887436d22bfc0c1e813207eecadf568b6799084
bionicbread <christopheryu87@gmail.com>
2022-02-07 21:37:24 +0000

    Exercise 1 and 2 complete (Working with Files)

--- C#/Section 9 - Working with Files/Exercise1/Exercise1/Program.cs
+++ C#/Section 9 - Working with Files/Exercise1/Exercise1/Program.cs
@@ -10,7 +10,12 @@ namespace Exercise1
             //1 - Write a program that reads a text file and displays the number of words.
             // use "C:\temp\tutorialTest"
 
-            string contentsOfFile = File.ReadAllText(@"C:\temp\tutorialTest");
+            string contentsOfFile = File.ReadAllText(@"C:\temp\tutorialTest\someFile.txt");
+
+            string[] numberOfWords = contentsOfFile.Split(' ');
+            Console.WriteLine(numberOfWords.Length);
+
+
 
         }
     }
---
+++ C#/Section 9 - Working with Files/Exericse 2/Exericse 2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace Exericse_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //2- Write a program that reads a text file and displays the longest word in the file.
+            // use C:\temp\tutorialTest\someFile.txt
+
+            string textFile = File.ReadAllText(@"C:\temp\tutorialTest\someFile.txt");
+
+            string[] words = textFile.Split(' ');
+            int wordSize = 0;
+            string largestWord = null;
+            foreach (string s in words)
+            {
+               
+                if (s.Length > wordSize)
+                {
+                    largestWord = s;
+                    wordSize = s.Length;
+                }
+                else if (s.Length < wordSize)
+                {
+                    continue;
+                }
+            }
+
+            Console.WriteLine(largestWord);
+        }
+    }
+}
