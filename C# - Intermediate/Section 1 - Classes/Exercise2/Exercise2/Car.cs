b5a4ba13c1f9c0db345cc4f56100c6bef876d1b2
bionicbread <christopheryu87@gmail.com>
2022-02-16 19:02:14 +0000

    Exercise 2 complete (Classes)

---
+++ C# - Intermediate/Section 1 - Classes/Exercise2/Exercise2/Car.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2
+{
+    class Car
+    {
+        private string _make;
+        private string _model;
+        private int _year;
+
+        public Car(string make, string model, int year)
+        {
+            this.Make = make;
+            this._model = model;
+            this._year = year;
+
+        }
+
+        public string Make 
+        {
+            get 
+            { 
+                return _make; 
+            }
+            set 
+            {
+                _make = value.ToLower();
+                
+            } 
+        }
+
+    }
+}
---
+++ C# - Intermediate/Section 1 - Classes/Exercise2/Exercise2/Post.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Text;
+
+namespace Exercise2
+{
+    class Post
+    {
+        private string _title;
+        private string _description;
+        private DateTime _timeOfPost;
+        private int _numberOfVotes;
+        private StringBuilder _mainPost = new StringBuilder();
+
+        public Post(string title, string description, DateTime dateTime, int voteNumber)
+        {
+            this.myTitle = title;
+            this.myDescription = description;
+            this.myTimeOfPost = dateTime;
+            this.myNumberOfVotes = voteNumber;
+        }
+
+
+        public string myTitle 
+        {
+            get 
+            { 
+                return _title; 
+            }
+            set 
+            {
+                _title = value;
+            } 
+        }
+        public string myDescription
+        {
+            get 
+            { 
+                return _description; 
+            }
+            set 
+            {
+                _description = value;
+            } 
+        }
+        public DateTime myTimeOfPost
+        {
+            get
+            {
+                return _timeOfPost;
+            }
+            set
+            {
+                _timeOfPost = value;
+            }
+        }
+        public int myNumberOfVotes
+        {
+            get
+            {
+                return _numberOfVotes;
+            }
+            set
+            {
+                _numberOfVotes = value;
+            }
+        }
+       
+
+        public void DisplyPost()
+        {
+            _mainPost.Append('-', _title.Length);
+            _mainPost.AppendLine();
+            _mainPost.Append(_title);
+            _mainPost.AppendLine();
+            _mainPost.Append('-', _title.Length);
+            _mainPost.AppendLine();
+            _mainPost.AppendLine();
+            _mainPost.Append(_description);
+            _mainPost.AppendLine();
+            _mainPost.Append('-', _description.Length);
+            _mainPost.AppendLine();
+            _mainPost.Append("Created "+ _timeOfPost + " | Number of Votes: " + _numberOfVotes);
+            Console.WriteLine(_mainPost);
+            
+        }
+
+        public void ChangeVote(string voteChoice)
+        {
+            if (voteChoice == "UP")
+            {
+                _numberOfVotes++;
+                Console.WriteLine(" ");
+            }
+            else if (voteChoice == "DOWN")
+            {
+                _numberOfVotes--;
+                Console.WriteLine(" ");
+            }
+            
+        }
+    }
+}
