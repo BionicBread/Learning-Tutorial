824c10f3b75f1117497fae3eb1e40741b39d8772
Chris Yu <biohazard_99@hotmail.com>
2022-02-23 16:08:27 +0000

    Exercise 1 complete

--- C# - Intermediate/Section 3 - Inheritance/Exercise1/Exercise1/Program.cs
+++ C# - Intermediate/Section 3 - Inheritance/Exercise1/Exercise1/Program.cs
@@ -6,7 +6,28 @@ namespace Exercise1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Stack stack = new Stack();
+
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(DateTime.Now);
+
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+
+            Console.WriteLine("Input 'Clear' to clear the stack or 'Quit' to exit");
+            string input = Console.ReadLine();
+            if (input == "Clear")
+            {
+                stack.Clear();
+            }
+            else if (input == "Quit")
+            {
+                Environment.Exit(0);
+            }
+
+            
         }
     }
 }
---
+++ C# - Intermediate/Section 3 - Inheritance/Exercise1/Exercise1/Stack.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+namespace Exercise1
+{
+    public class Stack
+    {
+        // contructor should init an array list or generic list
+        private ArrayList list = new ArrayList();  //initiate the array
+        public void Push(Object obj) // stores the given object at the top of the stack
+        {
+            if (obj != null)
+                list.Add(obj);
+            else
+                throw new NullReferenceException();
+        }
+
+        public void Clear() // clears the stack
+        {
+            list.Clear();
+            
+        }
+
+        public Object Pop() // returns the first element in the stack
+        {
+            var top = list[list.Count - 1];
+            list.Remove(list[list.Count -1]);
+            return top;
+      
+        }     
+    }
+}
