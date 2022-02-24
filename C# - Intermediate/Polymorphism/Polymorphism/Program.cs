44a98005f572d38c62bff7706c4070a236efc4e7
BionicBread <78957118+BionicBread@users.noreply.github.com>
2022-02-24 13:56:05 +0000

    Add Method Overriding

---
+++ C# - Intermediate/Polymorphism/Polymorphism/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Method overriding 
+            // Java's final keyword 
+        }
+    }
+}
