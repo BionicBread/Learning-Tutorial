1cff99290e81bb1fcdf4ee50bad7765e863265fe
BionicBread <78957118+BionicBread@users.noreply.github.com>
2022-03-06 22:13:58 +0000

    Interface 2

--- C# - Intermediate/Section 5 - Interfaces/Interfaces2/Interfaces2/DbMigrator.cs
+++ C# - Intermediate/Section 5 - Interfaces/Interfaces2/Interfaces2/DbMigrator.cs
@@ -2,10 +2,11 @@
 
 namespace Interfaces2
 {
+    
     public class DbMigrator
     {
-        private readonly ILogger logger;
-
+        
+        private readonly ILogger logger; 
         
         public DbMigrator(ILogger logger) // Dependancy injection - in the contructor the dependancies is specified
         {
@@ -13,8 +14,13 @@ namespace Interfaces2
         }
         public void Migrate()
         {
-            logger.LogInfo("Migration Started at {0}" + DateTime.Now);
+            logger.LogInfo("Migration Started at {0}" + DateTime.Now); // use the interface instead of console
             
         }
+        //public void Mirgrate() // Removed for interface implementation
+        //{
+        //    Console.WriteLine("Migration started.....at {0}", DateTime.Now);
+        //    Console.WriteLine("Migration ended .....at {0}", DateTime.Now);
+        //}
     }
 }
